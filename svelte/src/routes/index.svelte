d7b9c9735f03d93e145fb24aea38ae4ec2ce2f07
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-05-29 19:23:04 +0200

    Add footer with reddit link to me

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -6,7 +6,6 @@
   const REMEMBERME_KEY = "urn:getmydeck:rememberme";
   
   let reservationTime: string;
-  let reservationTimeHuman;
   let selectedRegion: string;
   let selectedVersion: string;
   let rememberme = false;
@@ -88,17 +87,21 @@
     <article class="">
       <h1 class="text-center">How long to get my Steam Deck?</h1>
       <p>Inspired from the reddit Steam Deck Order Email Megathreads <a target="_blank" href="https://www.reddit.com/r/SteamDeck/search?q=flair_name%3A%22MEGATHREAD%22&restrict_sr=1">(list of them here)</a> 
-        I wanted to give an alternative way of getting actual 
+        I wanted to give an alternative way of getting actual information
         besides talking to the deckbot within the reddit. 
       </p>
       <p>
-        Anyway thanks to the guys behind the thread and there work on organizing this and making the data available. 
-        This site also relies on the data the people share in the reddit to collect information which people are able to
+        Anyway thanks to the great work of all guys behind the thread and their work on organizing this 
+        and making the data available. 
+        This site also relies on the data the people share in the reddit to 
+        collect information which people are able to
         order their steam deck based on the reservation time, their region and their version.
       </p>
       <p>
-        Here you just need to enter your data to get your information about how far away you might be from ordering your steam deck. Every week 
-        you can just come back and see if something has changed (without a reddit account and talking to deckbot every week).
+        Here you just need to enter your data to get your information about how far 
+        away you might be from ordering your steam deck. Every week 
+        you can just come back and see if something has changed (without a reddit 
+        account and talking to deckbot every week).
       </p>
     </article>
 
@@ -144,7 +147,8 @@
 
     </form>
     
-    <div class="block mt-3 pt-3 border-t-2 prose-lg">
+    <div class="block border-t-2">
+      <h3>Results:</h3>
       {#if showValidationError }
       <p>Please fill out form completely</p>
       {/if}
@@ -153,13 +157,15 @@
           <p>{errorMessage}</p>
         {:else}
           {#if deckdata}
-            <h2>Results:</h2>
             <p>{deckdata.personalInfo.prettyText}</p>
           {:else}
             <p>Fetching infos ...</p>
           {/if}
         {/if}
       {/if}
+    </div>
+    <div class="block border-t-2">
+      <p>If you have questions or comments feel free to reach out for me on reddit <a target="_blank" href="https://www.reddit.com/u/Labidou51">Labidou51</a></p>
     </div>
   </div>
 </div>

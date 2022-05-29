37cfb6ee3d7339f57b1bccdf519ca57e80750cf3
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-05-29 10:11:27 +0200

    Some more information

--- src/main/java/de/ingenhaag/getmydeck/services/DeckService.java
+++ src/main/java/de/ingenhaag/getmydeck/services/DeckService.java
@@ -27,11 +27,15 @@ public class DeckService {
     personalInfo.setDurationReservedAfterStartHumanReadable(humanReadableDuration(getDurationBetweenStartAndPersonalReservation(reservedAt)));
     personalInfo.setElapsedTimePercentage(calculateElapsedTimePercentage(reservedAt, region, version));
     personalInfo.setPrettyText(
-        String.format("Hello! It looks like you have a %s %sGB reservation. You reserved your deck %s after pre-orders opened. You're %s of the way there.",
+        String.format("""
+            Hello! It looks like you have a %s %sGB reservation. 
+            You reserved your deck %s after pre-orders opened. 
+            You're %s of the way there because the last orders with your configuration did their reservation %s before you did.""",
             region,
             version.getVersion(),
             personalInfo.getDurationReservedAfterStartHumanReadable(),
-            personalInfo.getElapsedTimePercentage()));
+            personalInfo.getElapsedTimePercentage(),
+            calculateDurationBetweenLastShipmentAndMyReservation(reservedAt, region, version)));
 
     InfoResponse info = new InfoResponse();
     info.setOfficialInfo(officialInfo);
@@ -43,6 +47,12 @@ public class DeckService {
     return Duration.between(config.getReservationStart(), reservedAt);
   }
 
+  private String calculateDurationBetweenLastShipmentAndMyReservation(OffsetDateTime reservedAt, Region region, Version version) {
+    OffsetDateTime myLastShipment = config.getLastShipments().get(region).get(version);
+    Duration d = Duration.between(myLastShipment, reservedAt);
+
+    return humanReadableDuration(d);
+  }
   private String calculateElapsedTimePercentage(OffsetDateTime reservedAt, Region region, Version version) {
     Duration diffToMyPersonalReservation = getDurationBetweenStartAndPersonalReservation(reservedAt);
 
@@ -58,7 +68,7 @@ public class DeckService {
     long HH = duration.toHoursPart();
     long MM = duration.toMinutesPart();
     long SS = duration.toSecondsPart();
-    return String.format("%02d days, %02d hours ,%02d minutes and %02d seconds", d, HH, MM, SS);
+    return String.format("%02d days, %02d hours, %02d minutes and %02d seconds", d, HH, MM, SS);
   }
 
   public OffsetDateTime getSelectedDeckLastShipment(Region region, Version version) {

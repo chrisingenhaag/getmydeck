608d90ca43e92204445daa0bf32989e8fbcdfcbc
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-05-30 23:49:26 +0200

    Add deckbot data fetcher

--- src/main/java/de/ingenhaag/getmydeck/GetmydeckApplication.java
+++ src/main/java/de/ingenhaag/getmydeck/GetmydeckApplication.java
@@ -2,8 +2,10 @@ package de.ingenhaag.getmydeck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GetmydeckApplication {
 
 	public static void main(String[] args) {

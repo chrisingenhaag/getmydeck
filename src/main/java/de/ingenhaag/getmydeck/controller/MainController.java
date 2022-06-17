5c0c8b4848249bfd0c800d7c6aad318ba0d78ab2
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-06-17 17:10:00 +0200

    Add catch all for svelte spa

--- src/main/java/de/ingenhaag/getmydeck/controller/MainController.java
+++ src/main/java/de/ingenhaag/getmydeck/controller/MainController.java
@@ -1,11 +1,15 @@
 package de.ingenhaag.getmydeck.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class MainController {
-
-  @GetMapping("")
+  @RequestMapping(value = {
+      "/{path:^(?!api)[^\\.]*}",
+      "/**/{path:^(?!api).*}/{path:[^\\.]*}"}
+  )
   public String index(Model model) {
     return "index";
   }

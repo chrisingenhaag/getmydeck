55dcbe2f34a3b55719705f7bc93df4f3d4dfffaf
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-06-17 23:13:23 +0200

    Toggles for graph

--- svelte/src/lib/DeckChart.svelte
+++ svelte/src/lib/DeckChart.svelte
@@ -72,17 +72,79 @@
 </script>
 
 {#if chartData}
-  <span>
-    Show all data
-    <button on:click={toggleShowAllData}>All</button>
-  </span>
-  <span>
-    Show all data
-    <button on:click={toggleFullYScale}>Scale</button>
-  </span>
   <Chart data={chartData} type="line" 
-      lineOptions={chartLineOptions} 
-      tooltipOptions={chartTooltipOptions} 
-      valuesOverPoints="1"/>
+    lineOptions={chartLineOptions} 
+    tooltipOptions={chartTooltipOptions} 
+    valuesOverPoints="1"/>
+  <div class="object-right">
+    <span>
+      Show all data
+      <label class="switch">
+        <input type="checkbox" on:click={toggleShowAllData} class="w-0 h-0 opacity-0">
+        <span class="slider round rounded-2xl"></span>
+      </label>
+    </span>
+    <span>
+      / 
+      Full % scale
+      <label class="switch">
+        <input type="checkbox" on:click={toggleFullYScale} class="w-0 h-0 opacity-0">
+        <span class="slider round rounded-2xl"></span>
+      </label>
+    </span>
+  </div>
 {/if}
+
+<style>
+  /* The switch - the box around the slider */
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 30px;
+    height: 17px;
+  }
+
+  /* The slider */
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 13px;
+    width: 13px;
+    left: 2px;
+    bottom: 2px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+
+  input:checked + .slider {
+    background-color: #2196F3;
+  }
+
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196F3;
+  }
+
+  input:checked + .slider:before {
+    -webkit-transform: translateX(13px);
+    -ms-transform: translateX(13px);
+    transform: translateX(13px);
+  }
+
+  .slider.round:before {
+    border-radius: 50%;
+  } 
+</style>
 

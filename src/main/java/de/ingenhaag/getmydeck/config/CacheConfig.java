8019c5b202ed838cbdfe5ba04871a49895812c21
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-08-04 16:48:01 +0200

    Remove caching

--- src/main/java/de/ingenhaag/getmydeck/config/CacheConfig.java
+++
@@ -1,9 +0,0 @@
-package de.ingenhaag.getmydeck.config;
-
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableCaching
-public class CacheConfig {
-}

26528fa2fc2c815662b08c79dec8e6d7f6fd9216
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-08-11 19:36:49 +0200

    Add wating for log message mongodb startup

--- src/test/java/de/ingenhaag/getmydeck/testsupport/AbstractMongoContainerIntegrationTest.java
+++ src/test/java/de/ingenhaag/getmydeck/testsupport/AbstractMongoContainerIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -28,7 +29,7 @@ public class AbstractMongoContainerIntegrationTest {
         DockerImageName.parse("bitnami/mongodb:latest")
     )
         .withExposedPorts(27017)
-        .withStartupTimeout(Duration.ofMinutes(3))
+        .waitingFor(Wait.forLogMessage(".*Waiting for connections.*\\n", 1))
         .withEnv(Map.of("MONGODB_ROOT_PASSWORD", "password123",
             "MONGODB_USERNAME", "getmydeck",
             "MONGODB_PASSWORD", "password123",

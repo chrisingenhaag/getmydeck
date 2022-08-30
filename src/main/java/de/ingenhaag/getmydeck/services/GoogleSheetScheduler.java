e54a704064731c432f1ca9931f50ebb817e9533e
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-08-30 15:00:52 +0200

    Fix and add working counter on scheduler

--- src/main/java/de/ingenhaag/getmydeck/services/GoogleSheetScheduler.java
+++ src/main/java/de/ingenhaag/getmydeck/services/GoogleSheetScheduler.java
@@ -4,7 +4,7 @@ import de.ingenhaag.getmydeck.config.DeckBotConfiguration;
 import de.ingenhaag.getmydeck.models.deckbot.Region;
 import de.ingenhaag.getmydeck.models.deckbot.Version;
 import de.ingenhaag.getmydeck.models.google.DeckBotSheetResponse;
-import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +13,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -32,45 +33,53 @@ public class GoogleSheetScheduler {
   private final RestTemplate restTemplate;
   private final DeckBotConfiguration deckBotConfiguration;
   private SteamDeckMongoService mongoPersistenceService;
+  private MeterRegistry meterRegistry;
 
   @Autowired
   public GoogleSheetScheduler(RestTemplateBuilder builder,
                               DeckBotConfiguration deckBotConfiguration,
-                              SteamDeckMongoService mongoPersistenceService) {
+                              SteamDeckMongoService mongoPersistenceService,
+                              MeterRegistry meterRegistry) {
     this.restTemplate = builder.build();
     this.deckBotConfiguration = deckBotConfiguration;
     this.mongoPersistenceService = mongoPersistenceService;
+    this.meterRegistry = meterRegistry;
   }
 
   @Scheduled(timeUnit = TimeUnit.MINUTES, fixedRate = 1, initialDelay = 2)
-  @Counted(description = "measure background scheduled task to fetch data from fammys google sheet via rest and store it to mongodb")
   protected void fetchDeckBotData() {
-    final ResponseEntity<DeckBotSheetResponse> response = restTemplate.getForEntity(URI.create(deckBotConfiguration.getUrl()), DeckBotSheetResponse.class);
-    if (response.hasBody() && response.getBody() != null) {
-      try {
-        SortedMap<Region, SortedMap<Version, OffsetDateTime>> parsedData = new TreeMap<>();
-        response.getBody().getValues()
-            .forEach(strings -> {
-              Version version = Version.valueOfSize(strings.get(0));
-              Region region = Region.valueOf(strings.get(1));
-              OffsetDateTime stamp = OffsetDateTime.ofInstant(Instant.ofEpochSecond(Long.parseLong(strings.get(2))), ZoneOffset.UTC);
+    try {
+      final ResponseEntity<DeckBotSheetResponse> response = restTemplate.getForEntity(URI.create(deckBotConfiguration.getUrl()), DeckBotSheetResponse.class);
+      if (response.hasBody() && response.getBody() != null) {
+        try {
+          SortedMap<Region, SortedMap<Version, OffsetDateTime>> parsedData = new TreeMap<>();
+          response.getBody().getValues()
+              .forEach(strings -> {
+                Version version = Version.valueOfSize(strings.get(0));
+                Region region = Region.valueOf(strings.get(1));
+                OffsetDateTime stamp = OffsetDateTime.ofInstant(Instant.ofEpochSecond(Long.parseLong(strings.get(2))), ZoneOffset.UTC);
 
-              if (parsedData.containsKey(region)) {
-                parsedData.get(region).put(version, stamp);
-              } else {
-                SortedMap<Version, OffsetDateTime> versionOffsetDateTimeMap = new TreeMap<>();
-                versionOffsetDateTimeMap.put(version, stamp);
-                parsedData.put(region, versionOffsetDateTimeMap);
-              }
-            });
-        mongoPersistenceService.updateParsedDataIfChanged(parsedData);
-      } catch(NullPointerException e) {
-        log.error("Error parsing response from googlesheet", e);
-        throw new RuntimeException(e);
+                if (parsedData.containsKey(region)) {
+                  parsedData.get(region).put(version, stamp);
+                } else {
+                  SortedMap<Version, OffsetDateTime> versionOffsetDateTimeMap = new TreeMap<>();
+                  versionOffsetDateTimeMap.put(version, stamp);
+                  parsedData.put(region, versionOffsetDateTimeMap);
+                }
+              });
+          mongoPersistenceService.updateParsedDataIfChanged(parsedData);
+          meterRegistry.counter("getmydeck.scheduler.executions", "outcome", "SUCCESS", "exception", "NONE").increment();
+        } catch(NullPointerException e) {
+          log.error("Error parsing response from googlesheet", e);
+          meterRegistry.counter("getmydeck.scheduler.executions", "outcome", "FAILED", "exception", e.getCause().toString()).increment();
+        }
+      } else {
+        log.error("Google sheet response evaluated to null with response {}", response);
+        meterRegistry.counter("getmydeck.scheduler.executions", "outcome", "FAILED", "exception", "NONE").increment();
       }
-    } else {
-      log.error("Google sheet response evaluated to null with response {}", response);
-      throw new RuntimeException("Google sheet no body or null");
+    } catch (RestClientException exception) {
+      log.error("RestClientException during google sheet sync scheduling", exception);
+      meterRegistry.counter("getmydeck.scheduler.executions", "outcome", "FAILED", "exception", exception.getCause().toString()).increment();
     }
   }
 }
